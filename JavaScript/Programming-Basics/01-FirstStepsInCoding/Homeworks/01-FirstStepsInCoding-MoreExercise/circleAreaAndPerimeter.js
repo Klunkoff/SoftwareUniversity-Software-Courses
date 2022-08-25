c02a36c70b2d44c182aacbcaf12eed3919c5f3f6
Dimitar Klunkoff <klunkoff@gmail.com>
2022-08-25 20:02:41 +0300

    Create circleAreaAndPerimeter.js

---
+++ JavaScript/Programming-Basics/01-FirstStepsInCoding/Homeworks/01-FirstStepsInCoding-MoreExercise/circleAreaAndPerimeter.js
@@ -0,0 +1,15 @@
+function solve(input) {
+
+    let r = Number(input[0]);
+
+    let area = Math.PI * Math.pow(r, 2);
+
+    let perimeter = (Math.PI * r) * 2;
+
+    console.log(area.toFixed(2));
+    console.log(perimeter.toFixed(2));
+
+}
+
+// solve([3]);
+solve([4.5]);
