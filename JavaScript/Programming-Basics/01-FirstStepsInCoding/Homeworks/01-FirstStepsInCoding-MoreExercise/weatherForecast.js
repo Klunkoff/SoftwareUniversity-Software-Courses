f68b18509ba21740cdd61a5098b956bf95d1fc61
Dimitar Klunkoff <klunkoff@gmail.com>
2022-08-26 18:41:41 +0300

    Create weatherForecast.js

---
+++ JavaScript/Programming-Basics/01-FirstStepsInCoding/Homeworks/01-FirstStepsInCoding-MoreExercise/weatherForecast.js
@@ -0,0 +1,15 @@
+function solve(input) {
+
+    let weather = input[0];
+
+    switch (weather) {
+
+        case 'sunny': console.log(`It's warm outside!`); break;
+
+        default: console.log(`It's cold outside!`); break;
+    }
+}
+
+// solve(['sunny']);
+solve(['cloudy']);
+// solve(['snowy']);
