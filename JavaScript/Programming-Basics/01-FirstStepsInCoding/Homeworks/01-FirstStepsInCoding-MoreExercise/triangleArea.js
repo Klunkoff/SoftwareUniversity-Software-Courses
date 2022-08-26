758b1c78eddf0e908ea44309d45c70460ffe0f3d
Dimitar Klunkoff <klunkoff@gmail.com>
2022-08-26 18:38:03 +0300

    Create triangleArea.js

---
+++ JavaScript/Programming-Basics/01-FirstStepsInCoding/Homeworks/01-FirstStepsInCoding-MoreExercise/triangleArea.js
@@ -0,0 +1,12 @@
+function solve(input) {
+
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+
+    let result = a * b / 2;
+
+    console.log(result.toFixed(2));
+
+}
+
+solve(['15', '35',]);
