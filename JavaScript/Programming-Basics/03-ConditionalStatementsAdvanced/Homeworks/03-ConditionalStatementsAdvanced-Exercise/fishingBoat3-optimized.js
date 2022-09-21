8e520087e10d9f347bb1423edc562d2a5a0dfe86
Dimitar Klunkoff <klunkoff@gmail.com>
2022-09-21 19:40:49 +0300

    Create fishingBoat3-optimized.js

---
+++ JavaScript/Programming-Basics/03-ConditionalStatementsAdvanced/Homeworks/03-ConditionalStatementsAdvanced-Exercise/fishingBoat3-optimized.js
@@ -0,0 +1,38 @@
+function solve(input) {
+
+    let budget = Number(input[0]);
+    let season = input[1];
+    let fishermens = Number(input[2]);
+  
+    let shipRent = 0;
+  
+    switch (season) {
+     
+        case "Spring": shipRent = 3000; break;
+        case "Summer":
+        case "Autumn": shipRent = 4200; break;
+        case "Winter": shipRent = 2600; break;
+    }
+
+    if (fishermens <= 6) {
+        shipRent = shipRent - shipRent * 0.1;
+      } else if (fishermens >= 7 && fishermens <= 11) {
+        shipRent = shipRent - shipRent * 0.15;
+      } else {
+        shipRent = shipRent - shipRent * 0.25;
+      }
+  
+    if (fishermens % 2 === 0 && season !== 'Autumn'){
+      shipRent = shipRent - (shipRent * 0.05)
+    }
+  
+    if (shipRent <= budget) {
+      let result = budget - shipRent;
+      console.log(`Yes! You have ${result.toFixed(2)} leva left.`);
+    } else {
+      let result = shipRent - budget;
+      console.log(`Not enough money! You need ${result.toFixed(2)} leva.`);
+    }
+  }
+  
+  solve(["2000", "Winter", "13"]);
