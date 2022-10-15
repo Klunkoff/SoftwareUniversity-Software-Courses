cc2c244dd4baafef54a65b8d352fe137287fc0dd
Dimitar Klunkoff <klunkoff@gmail.com>
2022-10-15 11:30:53 +0300

    Create numbersDivisibleBy9-1.js

---
+++ JavaScript/Programming-Basics/04-ForLoop/Homeworks/04-ForLoop-Lab/numbersDivisibleBy9-1.js
@@ -0,0 +1,27 @@
+function solve(input) {
+
+  let startInterval = Number(input[0]);
+  let finishInterval = Number(input[1]);
+
+  let sum = 0;
+
+  for (let i = startInterval; i <= finishInterval; i++) {
+
+    if (i % 9 === 0) {
+
+      sum += i;
+    }
+  }
+
+  console.log(`The sum: ${sum}`);
+
+  for (let i = startInterval; i <= finishInterval; i++) {
+
+    if (i % 9 === 0) {
+
+      console.log(i);
+    }
+  }
+}
+
+solve(['100', '200']);
