c46b9e5807da48ffe6a1a2511f1b6319caaa2a9c
Dimitar Klunkoff <klunkoff@gmail.com>
2022-10-18 19:59:57 +0300

    Create histrogram-1.js

---
+++ JavaScript/Programming-Basics/04-ForLoop/Homeworks/04-ForLoop-Exercise/histrogram-1.js
@@ -0,0 +1,41 @@
+function solve(input) {
+
+    let n = Number(input[0]);
+
+    let p1 = 0;
+    let p2 = 0;
+    let p3 = 0;
+    let p4 = 0;
+    let p5 = 0;
+
+    for (let i = 1; i <= n; i++) {
+
+        let countNum = Number(input[i]);
+
+        if (countNum < 200) {
+            p1++;
+        } else if (countNum >= 200 && countNum < 400) {
+            p2++;
+        } else if (countNum >= 400 && countNum < 600) {
+            p3++;
+        } else if (countNum >= 600 && countNum < 800) {
+            p4++;
+        } else if (countNum >= 800) {
+            p5++;
+        }
+    }
+
+    let percentp1 = p1 / n * 100;
+    let percentp2 = p2 / n * 100;
+    let percentp3 = p3 / n * 100;
+    let percentp4 = p4 / n * 100;
+    let percentp5 = p5 / n * 100;
+
+    console.log(`${percentp1.toFixed(2)}%`);
+    console.log(`${percentp2.toFixed(2)}%`);
+    console.log(`${percentp3.toFixed(2)}%`);
+    console.log(`${percentp4.toFixed(2)}%`);
+    console.log(`${percentp5.toFixed(2)}%`);
+}
+
+solve(["7", "800", "801", "250", "199", "399", "599", "799"]);
