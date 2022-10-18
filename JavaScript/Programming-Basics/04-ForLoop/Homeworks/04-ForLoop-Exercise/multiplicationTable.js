cefa8cf91f8e1a0a566cdb5dd5079aff3a07f341
Dimitar Klunkoff <klunkoff@gmail.com>
2022-10-18 19:57:06 +0300

    Create multiplicationTable.js

---
+++ JavaScript/Programming-Basics/04-ForLoop/Homeworks/04-ForLoop-Exercise/multiplicationTable.js
@@ -0,0 +1,13 @@
+function solve(input) {
+
+    let number = Number(input[0]);
+
+    for (let i = 1; i <= 10; i++) {
+
+        let result = i * number;
+
+        console.log(`${i} * ${number} = ${result}`);
+    }
+}
+
+solve(['5']);
