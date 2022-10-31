b6514ee5da8c7d6b2d08b04cee13aa25963e5b78
Dimitar Klunkoff <klunkoff@gmail.com>
2022-10-31 18:36:57 +0200

    Create oscars.js

---
+++ JavaScript/Programming-Basics/04-ForLoop/ExamTasks/oscars.js
@@ -0,0 +1,37 @@
+function solve(input) {
+
+    let actorName = input[0];
+    let academyScore = Number(input[1]);
+    let academyPeopleNum = Number(input[2]);
+
+    let result = 0;
+    let overalScore = academyScore;
+
+    let index = 3;
+
+    for (let i = 1; i <= academyPeopleNum; i++) {
+
+        let nameLength = input[index];
+        index++;
+        let nameScore = Number(input[index]);
+        index++;
+
+        result = nameLength.length * nameScore / 2;
+        overalScore += result;
+
+        if (overalScore > 1250.5) {
+
+            console.log(`Congratulations, ${actorName} got a nominee for leading role with ${(overalScore).toFixed(1)}!`);
+            
+            break;
+        }
+    }
+    if (overalScore < 1250.5) {
+        
+        let difference = 1250.5 - overalScore;
+
+        console.log(`Sorry, ${actorName} you need ${difference.toFixed(1)} more!`);
+    }
+}
+
+solve(["Zahari Baharov", "205", "4", "Johnny Depp", "45", "Will Smith", "29", "Jet Lee", "10", "Matthew Mcconaughey", "39"]);
