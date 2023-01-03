1d253f0afba985fc44d83f9c55e0b60953d6a3ff
Dimitar Klunkoff <klunkoff@gmail.com>
2023-01-03 20:42:23 +0200

    Create rectangleOf10X10Stars.js

---
+++ JavaScript/Programming-Basics/07-DrawingFiguresWithLoops/rectangleOf10X10Stars.js
@@ -0,0 +1,16 @@
+function solve() {
+ 
+    for (let i = 1; i <= 10; i++) {
+ 
+        let starRow = '';
+ 
+        for (let j = 1; j <= 10; j++) {
+ 
+            starRow += '*';
+        }
+ 
+        console.log(starRow);
+    }
+}
+ 
+solve();
