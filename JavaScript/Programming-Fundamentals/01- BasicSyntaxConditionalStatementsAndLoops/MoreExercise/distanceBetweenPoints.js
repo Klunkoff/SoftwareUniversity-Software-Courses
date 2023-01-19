37c2b6807035d317af569d6b94fe75adb615888e
Dimitar Klunkoff <klunkoff@gmail.com>
2023-01-19 19:47:31 +0200

    Create distanceBetweenPoints.js

---
+++ JavaScript/Programming-Fundamentals/01- BasicSyntaxConditionalStatementsAndLoops/MoreExercise/distanceBetweenPoints.js
@@ -0,0 +1,9 @@
+function distanceBetweenPoints(x1, y1, x2, y2) {
+
+    let result = Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2);
+
+    console.log(Math.sqrt(result));
+}
+
+distanceBetweenPoints(2, 4, 5, 0);
+distanceBetweenPoints(2.34, 15.66, -13.55, -2.9985);
