ad3792078a337a0f2cae3ea6b4ca28f3dfbc33ee
Dimitar Klunkoff <klunkoff@gmail.com>
2023-01-25 18:01:12 +0200

    Create charsToString.js

---
+++ JavaScript/Programming-Fundamentals/02-DataTypesAndVariables/Exercise/charsToString.js
@@ -0,0 +1,10 @@
+function charsToString(charA, charB, charC) {
+
+    let string = charA + charB + charC;
+
+    console.log(string);
+}
+
+charsToString('a', 'b', 'c');
+charsToString('%', '2', 'o');
+charsToString('1', '5', 'p');
