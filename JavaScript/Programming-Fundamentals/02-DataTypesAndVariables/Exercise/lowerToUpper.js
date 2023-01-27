93746ba2a0aca85ddb3cff6a34534f5509bdd4be
Dimitar Klunkoff <klunkoff@gmail.com>
2023-01-27 19:04:45 +0200

    Create lowerToUpper.js

---
+++ JavaScript/Programming-Fundamentals/02-DataTypesAndVariables/Exercise/lowerToUpper.js
@@ -0,0 +1,16 @@
+function lowerToUpper(chars) {
+
+    let result = '';
+
+    if (chars == chars.toUpperCase()) {
+        result = 'upper-case';
+
+    } else {
+        result = 'lower-case';
+    }
+
+    console.log(result);
+}
+
+lowerToUpper('L');
+lowerToUpper('f');
