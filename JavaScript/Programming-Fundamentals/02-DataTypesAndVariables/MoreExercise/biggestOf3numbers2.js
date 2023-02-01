4667eb7a1854a3c346bbc9bb2b4b0c6e551fe6fe
Dimitar Klunkoff <klunkoff@gmail.com>
2023-02-01 18:38:09 +0200

    Create biggestOf3numbers2.js

---
+++ JavaScript/Programming-Fundamentals/02-DataTypesAndVariables/MoreExercise/biggestOf3numbers2.js
@@ -0,0 +1,10 @@
+function biggestOf3Numbers(num1, num2, num3) {
+ 
+    let biggestNum = Math.max(num1, num2, num3);
+ 
+    console.log(biggestNum);
+}
+ 
+biggestOf3Numbers(-2, 7, 3);
+biggestOf3Numbers(130, 5, 99);
+biggestOf3Numbers(2, 2, 2);
