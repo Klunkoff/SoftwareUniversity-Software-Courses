d5e9e9cb81007e17b9d6a2a3e0581f93816b3950
Dimitar Klunkoff <klunkoff@gmail.com>
2023-02-28 17:52:26 +0200

    Create NxNMatrix.js

---
+++ JavaScript/Programming-Fundamentals/04-Functions/Exercise/NxNMatrix.js
@@ -0,0 +1,20 @@
+function NxNMatrix(number) {
+ 
+    let matrix = [];
+ 
+    for (let i = 0; i < number; i++) {
+        let row = [];
+ 
+        for (let j = 0; j < number; j++) {
+            row.push(number);
+        }
+ 
+        matrix.push(row.join(' '));
+    }
+    
+    console.log(matrix.join('\n'));
+}
+ 
+NxNMatrix(3);
+// NxNMatrix(7);
+// NxNMatrix(2);
