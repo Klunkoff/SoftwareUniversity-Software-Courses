b86535a8da353e36efcf3c612ba0508d1cbdef53
Dimitar Klunkoff <klunkoff@gmail.com>
2023-03-14 19:46:02 +0200

    Create distinctArray.js

---
+++ JavaScript/Programming-Fundamentals/05-ArraysAdvanced/Exercise/distinctArray.js
@@ -0,0 +1,17 @@
+function distinctArray(arrayOfNumbers) {
+
+    let resultArray = [];
+
+    for (let element of arrayOfNumbers) {
+
+        if (!resultArray.includes(element)) {
+            resultArray.push(element);
+        }
+    }
+
+    console.log(resultArray.join(' '));
+}
+
+// distinctArray([1, 2, 3, 4]);
+// distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2]);
+distinctArray([20, 8, 12, 13, 4, 4, 8, 5]);
