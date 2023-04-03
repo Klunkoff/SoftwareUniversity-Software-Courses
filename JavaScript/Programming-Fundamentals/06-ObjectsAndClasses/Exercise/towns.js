b52d1a4840bdd5a548eebef2d8a05a0a2a2159f7
Dimitar Klunkoff <klunkoff@gmail.com>
2023-04-03 18:31:50 +0300

    Create towns.js

---
+++ JavaScript/Programming-Fundamentals/06-ObjectsAndClasses/Exercise/towns.js
@@ -0,0 +1,23 @@
+function towns(dataArray) {
+
+    let townObject = {};
+
+    for (let element of dataArray) {
+
+        let elementArray = element.split(' | ');
+
+        let currentTown = elementArray[0];
+        let currentLatitude = (Number(elementArray[1])).toFixed(2);
+        let currentLongitude = (Number(elementArray[2])).toFixed(2);
+
+        townObject.town = currentTown;
+        townObject.latitude = currentLatitude;
+        townObject.longitude = currentLongitude;
+
+        console.log(townObject);
+    }
+}
+
+towns(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625']);
+// towns(['Plovdiv | 136.45 | 812.575']);
