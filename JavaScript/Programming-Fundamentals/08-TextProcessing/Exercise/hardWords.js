4500647b5e01a08c1439f6be5b481378d2599266
Dimitar Klunkoff <klunkoff@gmail.com>
2023-04-23 00:26:34 +0300

    Create hardWords.js

---
+++ JavaScript/Programming-Fundamentals/08-TextProcessing/Exercise/hardWords.js
@@ -0,0 +1,29 @@
+function hardWords(inputData) {
+ 
+    let text = inputData.shift();
+    let wordsAray = inputData.shift();
+    let pattern = /_+/g;
+ 
+    let holesInText = pattern.exec(text);
+ 
+    while (holesInText != null) {
+ 
+        let currentHole = holesInText[0];
+ 
+        for (let word of wordsAray) {
+ 
+            if (word.length == currentHole.length) {
+                text = text.replace(currentHole, word);
+ 
+                break;
+            }
+        }
+ 
+        holesInText = pattern.exec(text);
+    }
+ 
+    console.log(text);
+}
+ 
+hardWords(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+    ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']])
