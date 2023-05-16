9ef69923abdbe2753bfa09852b9a96e3d386a8e6
Dimitar Klunkoff <klunkoff@gmail.com>
2023-05-16 18:27:20 +0300

    Create echoFunction.js

---
+++ JavaScript/JS-Advanced/01-SyntaxFunctionsAndStatements/Lab/echoFunction.js
@@ -0,0 +1,10 @@
+function echoFunction(stringElement) {
+
+    let stringElementLength = stringElement.length;
+
+    console.log(stringElementLength);
+    console.log(stringElement);
+}
+
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');
