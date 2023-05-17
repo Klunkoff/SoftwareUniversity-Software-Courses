a73fa9930d30f697d7dd4d1014509ed3c9d04344
Dimitar Klunkoff <klunkoff@gmail.com>
2023-05-17 21:46:11 +0300

    Create squareOfStars2.js

---
+++ JavaScript/JS-Advanced/01-SyntaxFunctionsAndStatements/Lab/squareOfStars2.js
@@ -0,0 +1,12 @@
+function squareOfStars(number = 5) {
+
+    for (let i = 0; i < number; i++) {
+
+        console.log('* '.repeat(number));
+    }
+}
+
+// squareOfStars(1);
+squareOfStars(12);
+// squareOfStars(5);
+// squareOfStars();
