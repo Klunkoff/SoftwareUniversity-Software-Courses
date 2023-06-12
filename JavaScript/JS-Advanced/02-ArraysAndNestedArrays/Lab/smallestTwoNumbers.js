be7a2c220a3f043c0fed01b2ae64ac2f957f59a3
Dimitar Klunkoff <klunkoff@gmail.com>
2023-06-12 08:58:35 +0300

    Create smallestTwoNumbers.js

---
+++ JavaScript/JS-Advanced/02-ArraysAndNestedArrays/Lab/smallestTwoNumbers.js
@@ -0,0 +1,10 @@
+function smallestTwoNumbers(arrayOfNums) {
+ 
+    let sortedNumsArray = arrayOfNums.sort((a, b) => a - b);
+    let twoSmallestElements = sortedNumsArray.slice(0, 2);
+ 
+    console.log(twoSmallestElements.join(' '));
+}
+ 
+smallestTwoNumbers([30, 15, 50, 5]);
+smallestTwoNumbers([3, 0, 10, 4, 7, 3]);
