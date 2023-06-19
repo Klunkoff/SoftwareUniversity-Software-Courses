7f800df4d203b61f228aae883ba17a58fed76cb8
Dimitar Klunkoff <klunkoff@gmail.com>
2023-06-19 08:39:14 +0300

    Create constructionCrew.js

---
+++ JavaScript/JS-Advanced/03-ObjectsAndComposition/Exercise/constructionCrew.js
@@ -0,0 +1,33 @@
+function constructionCrew(workerObject) {
+
+    if (workerObject.dizziness) {
+        calculateDizziness();
+    }
+
+   return workerObject;
+
+    function calculateDizziness() {
+
+       workerObject.levelOfHydrated += (workerObject.weight * 0.1) * workerObject.experience;
+        workerObject.dizziness = false;
+    };
+}
+
+console.log(constructionCrew({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+}));
+console.log(constructionCrew({
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true
+}));
+console.log(constructionCrew({
+    weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false
+}));
