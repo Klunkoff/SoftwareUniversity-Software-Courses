05394d80f19e65322c674f17bdc4c19abd32c7a4
Dimitar Klunkoff <klunkoff@gmail.com>
2023-06-26 19:37:27 +0300

    new task

---
+++ JavaScript/JS-Advanced/04-DOMIntroduction/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+ 
+    let hiddenText = document.getElementById('text');
+    hiddenText.style.display = 'inline';
+    let readMoreText = document.getElementById('more');
+    readMoreText.style.display = 'none';
+}
