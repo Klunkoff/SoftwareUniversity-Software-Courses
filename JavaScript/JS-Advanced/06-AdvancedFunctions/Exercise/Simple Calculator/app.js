134a028ea54104ac84c8118caa05564c5ef13732
Dimitar Klunkoff <klunkoff@gmail.com>
2023-07-16 20:41:54 +0300

    new task

---
+++ JavaScript/JS-Advanced/06-AdvancedFunctions/Exercise/Simple Calculator/app.js
@@ -0,0 +1,27 @@
+function calculator() {
+
+    return {
+
+        init(selector1, selector2, resultSelector) {
+
+            this.selector1 = document.querySelector(selector1);
+            this.selector2 = document.querySelector(selector2);
+            this.resultSelector = document.querySelector(resultSelector);
+        },
+
+        add() {
+
+            this.resultSelector.value = Number(this.selector2.value) + Number(this.selector1.value);
+        },
+
+        subtract() {
+
+            this.resultSelector.value = Number(this.selector1.value) - Number(this.selector2.value);
+        }
+    }
+}
+
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
+
+
